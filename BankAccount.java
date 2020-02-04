515810e123cc102a7ba9270ef7925f6ad154ffd8
12priya <priya.singla616@gmail.com>
2020-02-04 16:22:40 +0530

    day 2 code

--- BankAccount.java
+++ BankAccount.java
@@ -1,18 +1,70 @@
 public class BankAccount{
-    double balance=1000;
-    public double withdraw(double amount){
-        if(balance<=0)
+private double balance=10_000;
+    private Address address;
+    private AccountType accountType;
+private static int initial_account_id_value=1000;
+private int accountId;
+
+
+    public BankAccount(AccountType accountType,double initialBalance, Address address){
+
+        if(initialBalance>10000)
         {
-            System.out.println("insufficient balance");
+            this.balance=initialBalance;
         }
-            return balance-amount;
+        this.address=address;
+        this.accountType=accountType;
+        this.accountId=++initial_account_id_value;
+    }
+
+    public BankAccount(AccountType accountType,Address address)
+    {
+        this.address=address;
+        this.accountId=accountId;
+        this.accountType=accountType;
+    }
+
+    public double withdraw(double amount){
+
+        switch(this.accountType){
+            case SALARIED_ACCOUNT: this.balance-=amount;
+                                    break;
+                                    
+            case CURRENT_ACCOUNT: if((this.balance-amount)>25_000)
+            this.balance-=amount;
+            break;
+
+            case SAVINGS_ACCOUNT: if((this.balance-amount)>10_000)
+            this.balance-=amount;
+            break;
+        }
+        return amount;
+        // if((this.balance-amount)>10_000)
+        // {
+        //     this.balance-=amount;
+        //         return this.balance;
+        // }
+        //     return 0;
     }
 
     public double deposit(double amount){
-            return balance+amount;
+            return this.balance+=amount;
     }
 
     public double checkBalance(){
-        return balance;
+        return this.balance;
+    }
+    
+
+    public void updateAddress(Address address){
+        this.address=address;
+    }
+
+    public AccountType getAccountType(){
+        return this.accountType;
+    }
+
+    public static int getAccountID(){
+        return initial_account_id_value;
     }
 }
--- BankAccountClient.java
+++ BankAccountClient.java
@@ -1,27 +1,44 @@
 import java.util.*;
 public class BankAccountClient{
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Address address = new Address("202", "street 3", "BAthinda", 151001);
+        BankAccount bankaccount1=new BankAccount(AccountType.SAVINGS_ACCOUNT,20000,address);
+        BankAccount bankaccount2=new BankAccount(AccountType.SAVINGS_ACCOUNT,20000,address);
+        BankAccount bankaccount3=new BankAccount(AccountType.SAVINGS_ACCOUNT,20000,address);
+        BankAccount bankaccount4=new BankAccount(AccountType.SAVINGS_ACCOUNT,20000,address);
+        BankAccount bankaccount5=new BankAccount(AccountType.SAVINGS_ACCOUNT,20000,address);
+        BankAccount bankaccount6=new BankAccount(AccountType.SAVINGS_ACCOUNT,20000,address);
+
+        BankAccount bankaccount7=new BankAccount(AccountType.SAVINGS_ACCOUNT,20000,address);
+        BankAccount bankaccount8=new BankAccount(AccountType.SAVINGS_ACCOUNT,20000,address);
+
+        BankAccount bankaccount9=new BankAccount(AccountType.SAVINGS_ACCOUNT,20000,address);
+
+        BankAccount bankaccount10=new BankAccount(AccountType.SALARIED_ACCOUNT,address);
+System.out.println(bankaccount1.checkBalance());
+System.out.println(BankAccount.getAccountID());
+
+        // Scanner scan=new Scanner(System.in);
         
-        BankAccount ba=new BankAccount();
-        System.out.println("Enter 1 to withdraw\nEnter 2 to deposit\nEnter 3 to checkBalance ");
-        int n=scan.nextInt();
-        switch(n)
-        {
-            case 1:
-            System.out.println("enter the amount you want to withdraw");
-            double amountwithdrawn=scan.nextDouble();
-            System.out.println("amount left"+ba.withdraw(amountwithdrawn));
-            break;
-            case 2:
-            System.out.println("Enter the amount you want to deposit");
-            double depositamount=scan.nextDouble();
-           System.out.println("amount left"+ ba.deposit(depositamount));
-            break;
-            case 3: 
-          System.out.println(ba.checkBalance());
-            break;
-        }
+        // BankAccount ba=new BankAccount();
+        // System.out.println("Enter 1 to withdraw\nEnter 2 to deposit\nEnter 3 to checkBalance ");
+        // int n=scan.nextInt();
+        // switch(n)
+        // {
+        //     case 1:
+        //     System.out.println("enter the amount you want to withdraw");
+        //     double amountwithdrawn=scan.nextDouble();
+        //     System.out.println("amount left"+ba.withdraw(amountwithdrawn));
+        //     break;
+        //     case 2:
+        //     System.out.println("Enter the amount you want to deposit");
+        //     double depositamount=scan.nextDouble();
+        //    System.out.println("amount left"+ ba.deposit(depositamount));
+        //     break;
+        //     case 3: 
+        //   System.out.println(ba.checkBalance());
+        //     break;
+        // }
         
 
     }
